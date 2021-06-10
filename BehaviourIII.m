ca93fc038abdabc1af95e70a693dd5d5685fd57f
Carmen <kohl.carmen.1@gmail.com>
2021-06-10 09:02:52 -0400

    add confusion matrix

--- BehaviourIII.m
+++ BehaviourIII.m
@@ -69,7 +69,7 @@ for partic = 1:length(Partic)
             Data.(strcat('Session',num2str(session))).Tap_tms25_thr(pcount,:) = mean(Accuracy(CueCond==0 & TapCond==1 & TMSCond==2));
             Data.(strcat('Session',num2str(session))).Tap_tms25_spr(pcount,:) = mean(Accuracy(CueCond==0 & TapCond==2 & TMSCond==2));
             
-            % RT
+            % RT (correct)
             % tap only
             Data.(strcat('Session',num2str(session))).RT_Tap_pure_null(pcount,:) = mean(RT(CueCond==0 & TapCond==0 & TMSCond==0 & Accuracy==1))./1000;
             Data.(strcat('Session',num2str(session))).RT_Tap_pure_thr(pcount,:) = mean(RT(CueCond==0 & TapCond==1 & TMSCond==0 & Accuracy==1))./1000;
@@ -82,7 +82,33 @@ for partic = 1:length(Partic)
             Data.(strcat('Session',num2str(session))).RT_Tap_tms25_null(pcount,:) = mean(RT(CueCond==0 & TapCond==0 & TMSCond==2 & Accuracy==1))./1000;
             Data.(strcat('Session',num2str(session))).RT_Tap_tms25_thr(pcount,:) = mean(RT(CueCond==0 & TapCond==1 & TMSCond==2 & Accuracy==1))./1000;
             Data.(strcat('Session',num2str(session))).RT_Tap_tms25_spr(pcount,:) = mean(RT(CueCond==0 & TapCond==2 & TMSCond==2 & Accuracy==1))./1000;
-          
+           
+            % DETECTION CONFUSION MATRIX (only threshold tap/null tap)
+            %pure tap
+            Data.(strcat('Session',num2str(session))).ThrTap_ConfusionMatrix_NoTMS.TruePositive(pcount,:) = sum(CueCond==0 & TapCond==1 & TMSCond==0 & Accuracy ==1);
+            Data.(strcat('Session',num2str(session))).ThrTap_ConfusionMatrix_NoTMS.TrueNegative(pcount,:) = sum(CueCond==0 & TapCond==0 & TMSCond==0 & Accuracy ==1);
+            Data.(strcat('Session',num2str(session))).ThrTap_ConfusionMatrix_NoTMS.FalsePositive(pcount,:) = sum(CueCond==0 & TapCond==0 & TMSCond==0 & Accuracy ==0);
+            Data.(strcat('Session',num2str(session))).ThrTap_ConfusionMatrix_NoTMS.FalseNegative(pcount,:) = sum(CueCond==0 & TapCond==1 & TMSCond==0 & Accuracy ==0);
+            Data.(strcat('Session',num2str(session))).ThrTap_ConfusionMatrix_NoTMS.Sensitivity(pcount,:) = sum(CueCond==0 & TapCond==1 & TMSCond==0 & Accuracy ==1)/(sum(CueCond==0 & TapCond==1 & TMSCond==0 & Accuracy ==1) +sum(CueCond==0 & TapCond==1 & TMSCond==0 & Accuracy ==0)); %TP/(TP+FN)
+            Data.(strcat('Session',num2str(session))).ThrTap_ConfusionMatrix_NoTMS.Specificity(pcount,:) = sum(CueCond==0 & TapCond==0 & TMSCond==0 & Accuracy ==1)/(sum(CueCond==0 & TapCond==0 & TMSCond==0 & Accuracy ==1) + sum(CueCond==0 & TapCond==0 & TMSCond==0 & Accuracy ==0));%TN/(TN + FP)
+            Data.(strcat('Session',num2str(session))).ThrTap_ConfusionMatrix_NoTMS.Precision(pcount,:) = sum(CueCond==0 & TapCond==1 & TMSCond==0 & Accuracy ==1)/(sum(CueCond==0 & TapCond==1 & TMSCond==0 & Accuracy ==1) + sum(CueCond==0 & TapCond==0 & TMSCond==0 & Accuracy ==0));%TP/(TP+FP)
+            %tms100
+            Data.(strcat('Session',num2str(session))).ThrTap_ConfusionMatrix_TMS100.TruePositive(pcount,:) = sum(CueCond==0 & TapCond==1 & TMSCond==1 & Accuracy ==1);
+            Data.(strcat('Session',num2str(session))).ThrTap_ConfusionMatrix_TMS100.TrueNegative(pcount,:) = sum(CueCond==0 & TapCond==0 & TMSCond==1 & Accuracy ==1);
+            Data.(strcat('Session',num2str(session))).ThrTap_ConfusionMatrix_TMS100.FalsePositive(pcount,:) = sum(CueCond==0 & TapCond==0 & TMSCond==1 & Accuracy ==0);
+            Data.(strcat('Session',num2str(session))).ThrTap_ConfusionMatrix_TMS100.FalseNegative(pcount,:) = sum(CueCond==0 & TapCond==1 & TMSCond==1 & Accuracy ==0);
+            Data.(strcat('Session',num2str(session))).ThrTap_ConfusionMatrix_TMS100.Sensitivity(pcount,:) = sum(CueCond==0 & TapCond==1 & TMSCond==1 & Accuracy ==1)/(sum(CueCond==0 & TapCond==1 & TMSCond==0 & Accuracy ==1) +sum(CueCond==0 & TapCond==1 & TMSCond==0 & Accuracy ==0)); %TP/(TP+FN)
+            Data.(strcat('Session',num2str(session))).ThrTap_ConfusionMatrix_TMS100.Specificity(pcount,:) = sum(CueCond==0 & TapCond==0 & TMSCond==1 & Accuracy ==1)/(sum(CueCond==0 & TapCond==0 & TMSCond==0 & Accuracy ==1) + sum(CueCond==0 & TapCond==0 & TMSCond==0 & Accuracy ==0));%TN/(TN + FP)
+            Data.(strcat('Session',num2str(session))).ThrTap_ConfusionMatrix_TMS100.Precision(pcount,:) = sum(CueCond==0 & TapCond==1 & TMSCond==1 & Accuracy ==1)/(sum(CueCond==0 & TapCond==1 & TMSCond==0 & Accuracy ==1) + sum(CueCond==0 & TapCond==0 & TMSCond==0 & Accuracy ==0));%TP/(TP+FP)        
+            %tms25
+            Data.(strcat('Session',num2str(session))).ThrTap_ConfusionMatrix_TMS25.TruePositive(pcount,:) = sum(CueCond==0 & TapCond==1 & TMSCond==2 & Accuracy ==1);
+            Data.(strcat('Session',num2str(session))).ThrTap_ConfusionMatrix_TMS25.TrueNegative(pcount,:) = sum(CueCond==0 & TapCond==0 & TMSCond==2 & Accuracy ==1);
+            Data.(strcat('Session',num2str(session))).ThrTap_ConfusionMatrix_TMS25.FalsePositive(pcount,:) = sum(CueCond==0 & TapCond==0 & TMSCond==2 & Accuracy ==0);
+            Data.(strcat('Session',num2str(session))).ThrTap_ConfusionMatrix_TMS25.FalseNegative(pcount,:) = sum(CueCond==0 & TapCond==1 & TMSCond==2 & Accuracy ==0);
+            Data.(strcat('Session',num2str(session))).ThrTap_ConfusionMatrix_TMS25.Sensitivity(pcount,:) = sum(CueCond==0 & TapCond==1 & TMSCond==2 & Accuracy ==1)/(sum(CueCond==0 & TapCond==1 & TMSCond==0 & Accuracy ==1) +sum(CueCond==0 & TapCond==1 & TMSCond==0 & Accuracy ==0)); %TP/(TP+FN)
+            Data.(strcat('Session',num2str(session))).ThrTap_ConfusionMatrix_TMS25.Specificity(pcount,:) = sum(CueCond==0 & TapCond==0 & TMSCond==2 & Accuracy ==1)/(sum(CueCond==0 & TapCond==0 & TMSCond==0 & Accuracy ==1) + sum(CueCond==0 & TapCond==0 & TMSCond==0 & Accuracy ==0));%TN/(TN + FP)
+            Data.(strcat('Session',num2str(session))).ThrTap_ConfusionMatrix_TMS25.Precision(pcount,:) = sum(CueCond==0 & TapCond==1 & TMSCond==2 & Accuracy ==1)/(sum(CueCond==0 & TapCond==1 & TMSCond==0 & Accuracy ==1) + sum(CueCond==0 & TapCond==0 & TMSCond==0 & Accuracy ==0));%TP/(TP+FP)
+         
             
             %% TMS Detection
             % ACCURACY
@@ -220,7 +246,7 @@ for session = [0,1]
     b(2).FaceColor = [.5 .5 .5];
     b(3).FaceColor = [.3 .3 .3];
     
-     % errors
+    % errors
     sq = sqrt(length(data.Tms_null_null));
     error_toplot =[std(data.Tms_null_null)/sq,std(data.Tms_null_thr)/sq,std(data.Tms_null_spr)/sq;...
                    std(data.Tms_tms100_null)/sq,std(data.Tms_tms100_thr)/sq,std(data.Tms_tms100_spr)/sq;...
@@ -263,7 +289,7 @@ for session = [0,1]
     b(2).FaceColor = [.5 .5 .5];
     b(3).FaceColor = [.3 .3 .3];
     
-     % errors
+    % errors
     sq = sqrt(length(data.RT_Tms_null_null));
     error_toplot =[std(data.RT_Tms_null_null)/sq,std(data.RT_Tms_null_thr)/sq,std(data.RT_Tms_null_spr)/sq;...
                    std(data.RT_Tms_tms100_null)/sq,std(data.RT_Tms_tms100_thr)/sq,std(data.RT_Tms_tms100_spr)/sq;...
@@ -284,7 +310,98 @@ slide = add(ppt,"Title and Content");replace(slide,"Title",'RT - TMS Detection')
 print(tempfig,"-dpng",'tempfig4');Imageslide = Picture('tempfig4.png');
 replace(slide,"Content",Imageslide);     
             
+       
+
+%% Looking at detection more closely
+% COnfusion Matrix
+tempfig = figure('units','normalized','outerposition',[0 0 1 1]);
+count = 0;
+for dat = 1:3
+    for session = [0,1]
+        switch dat
+            case 1
+                data = Data.(strcat('Session',num2str(session))).ThrTap_ConfusionMatrix_NoTMS;
+                titl = strcat('Session ',num2str(session),'- Threshold Tap Detection - Tap Only');
+            case 2
+                data = Data.(strcat('Session',num2str(session))).ThrTap_ConfusionMatrix_TMS100;
+                titl = strcat('Session ',num2str(session),'- Threshold Tap Detection - TMS100');
+            case 3
+                data = Data.(strcat('Session',num2str(session))).ThrTap_ConfusionMatrix_TMS25;
+                titl = strcat('Session ',num2str(session),'- Threshold Tap Detection - TMS25');
+        end
+        count = count+1;
+        subplot(3,2,count)
+        toplot = [mean(data.TruePositive),mean(data.FalseNegative);...
+               mean(data.FalsePositive),mean(data.TrueNegative)];
+        cm = confusionchart(round(toplot),['O';'X'],'Title',titl,'GridVisible','off');
+        cm.XLabel = 'Participant Response';
+        cm.YLabel = 'Correct Response';
+        if dat ==1 
+            cm.DiagonalColor = [255,160,122]./255;
+            cm.OffDiagonalColor = [255,160,122]./255;
+        elseif dat==2
+            cm.DiagonalColor = [205,92,92]./255;
+            cm.OffDiagonalColor = [205,92,92]./255;
+        elseif dat==3
+            cm.DiagonalColor = [139,0,0]./255;
+            cm.OffDiagonalColor = [139,0,0]./255;
+        end
+    end
+end
+%ppt
+slide = add(ppt,"Title and Content");replace(slide,"Title",'Tap Confusion Matrix');
+print(tempfig,"-dpng",'tempfig5');Imageslide = Picture('tempfig5.png');
+replace(slide,"Content",Imageslide);     
+  
+% Confusion Summay
+tempfig = figure('units','normalized','outerposition',[0 0 1 1]);
+count = 0;
+for dat = 1:3
+    for session = [0,1]
+        switch dat
+            case 1
+                data = Data.(strcat('Session',num2str(session))).ThrTap_ConfusionMatrix_NoTMS;
+                titl = strcat('Session ',num2str(session),'- Threshold Tap Detection - Tap Only');
+            case 2
+                data = Data.(strcat('Session',num2str(session))).ThrTap_ConfusionMatrix_TMS100;
+                titl = strcat('Session ',num2str(session),'- Threshold Tap Detection - TMS100');
+            case 3
+                data = Data.(strcat('Session',num2str(session))).ThrTap_ConfusionMatrix_TMS25;
+                titl = strcat('Session ',num2str(session),'- Threshold Tap Detection - TMS25');
+        end
+        count = count+1;
+        subplot(3,2,count)
+        hold on
+        ylim([0 2])
+        toplot = [mean(data.Sensitivity),mean(data.Specificity),mean(data.Precision)];
+        b = bar(toplot);
+        if dat == 1
+            b.FaceColor = [255,160,122]./255;
+        elseif dat==2
+            b.FaceColor = [205,92,92]./255;
+        elseif dat==3
+            b.FaceColor = [139,0,0]./255;
+        end
+        
+        % errors
+        sq = sqrt(length(data.Sensitivity));
+        error_toplot =[std(data.Sensitivity)/sq,std(data.Specificity)/sq,std(data.Precision)/sq];
+        tick = 0.225;
+        err=errorbar(1:3,toplot,error_toplot);
+        err.Color = [0 0 0];                    
+        err.LineStyle = 'none';
+    
+        title(titl)
+        set(gca,'XTick',[1,2,3])
+        set(gca,'xticklabel',{['Sensitivity'],['Specificity'],['Precision']})
+    end
+end
+%ppt
+slide = add(ppt,"Title and Content");replace(slide,"Title",'Tap Confusion Matrix Summary');
+print(tempfig,"-dpng",'tempfig6');Imageslide = Picture('tempfig6.png');
+replace(slide,"Content",Imageslide);   
+
 
 close(ppt)
-delete tempfig1.png tempfig2.png tempfig3.png tempfig4.png      
-% close all           
+delete tempfig1.png tempfig2.png tempfig3.png tempfig4.png tempfig5.png tempfig6.png
+close all    
