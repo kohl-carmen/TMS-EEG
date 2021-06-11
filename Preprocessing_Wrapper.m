f997e8de286094d9bc28443d42390e0a432ebc34
Carmen <kohl.carmen.1@gmail.com>
2021-06-11 14:10:50 -0400

    print and save rejections

--- Preprocessing_Wrapper.m
+++ Preprocessing_Wrapper.m
@@ -1,10 +1,12 @@
 
 
-partic = 20,
+partic = 22,
 session = 1;
 
-cd('C:\Users\ckohl\Desktop\Current\TMS\TMS-EEG-AnalysisCode')
+
+
 %% Check Behaviour
+cd('C:\Users\ckohl\Desktop\Current\TMS\TMS-EEG-AnalysisCode')
 disp('========================')
 disp ('== Analysing Behaviour ==')
 disp('========================')
@@ -14,6 +16,7 @@ input(' Behaviour check done. Press Enter to continue.');
 close all
 
 %% NeuroNavigation
+cd('C:\Users\ckohl\Desktop\Current\TMS\TMS-EEG-AnalysisCode')
 disp('========================')
 disp ('== NeuroNavigation ==')
 disp('========================')
@@ -22,6 +25,7 @@ disp('========================')
 input(' NeuroNavigation check done. Press Enter to continue.');
 
 %% S1Loc
+cd('C:\Users\ckohl\Desktop\Current\TMS\TMS-EEG-AnalysisCode')
 disp('========================')
 disp ('== S1Loc ==')
 disp('========================')
@@ -30,7 +34,78 @@ Preprocessing_S1Loc(partic,session);
 input(' S1Loc preprocessing done. Press Enter to continue.');
 
 %% Task EEG
+cd('C:\Users\ckohl\Desktop\Current\TMS\TMS-EEG-AnalysisCode')
 disp('========================')
 disp ('== Task EEG ==')
 disp('========================')
-Preprocessing(partic, session, [bad_trials_mep;bad_trials_target]);
+[rej, common_trial_counter] = Preprocessing(partic, session, [bad_trials_mep;bad_trials_target]);
+
+%% Summary
+cd('C:\Users\ckohl\Desktop\Current\TMS\TMS-EEG-AnalysisCode')
+Trials = 720;
+partic_str = sprintf('%02d', partic);
+filedir = strcat('C:\Users\ckohl\Desktop\Data\BETA',partic_str,'\Session',num2str(session),'\');
+out_dir =  strcat(filedir,'\Preproc\');
+out_txt = fopen(strcat(out_dir,partic_str,'_preproc'), 'a');
+
+txt = sprintf('Trials deleted due to MEPs: %i (%2.2f%%)\n',length(bad_trials_mep), length(bad_trials_mep)/Trials*100);
+fprintf(out_txt, '%s\n ', txt); 
+fprintf('%s',txt)
+txt = sprintf('bad_trials_mep: %s\n',mat2str(bad_trials_mep));
+fprintf(out_txt, '%s\n ', txt); 
+
+txt = sprintf('Trials deleted due to failed Navigation: %i (%2.2f%%)\n',length(bad_trials_target), length(bad_trials_target)/Trials*100);
+fprintf(out_txt, '%s\n ', txt); 
+fprintf('%s',txt)
+txt = sprintf('bad_trials_target: %s\n',mat2str(bad_trials_target));
+fprintf(out_txt, '%s\n ', txt); 
+
+txt = sprintf('Trials deleted due to bad EEG data: %i (%2.2f%%)\n',length(rej), length(rej)/Trials*100);
+fprintf(out_txt, '%s\n ', txt); 
+fprintf('%s',txt)
+txt = sprintf('rej: %s\n',mat2str(rej));
+fprintf(out_txt, '%s\n ', txt); 
+
+txt = sprintf('Rejection overlap: %i trial(s)\n',common_trial_counter);
+fprintf(out_txt, '%s\n ', txt); 
+fprintf('%s',txt)
+all = length(bad_trials_mep) + length(bad_trials_target) +  length(rej) - common_trial_counter;
+txt = sprintf('Total trial loss: %i (%2.2f%%) - %i Trials remaining.\n',all, all/Trials*100, Trials-all);
+fprintf(out_txt, '%s\n ', txt); 
+fprintf('%s',txt)
+
+%delete from behavioural file
+Rejected = [bad_trials_mep,bad_trials_target,rej];
+Rejected = unique(Rejected);
+
+fid =fopen(strcat(filedir,'BETA',partic_str,'_results'));
+TXT = textscan(fid,'%s','delimiter','\n');
+fclose(fid);
+delete = [];
+for reject = Rejected
+    for row = 5:numel(TXT{1,1})
+        if TXT{1,1}{row,1}(1:numel(num2str(reject))) == num2str(reject)
+            break
+        end
+    end
+    delete = [delete, row];
+end
+% print new file
+fid =fopen(strcat(filedir,'BETA',partic_str,'_results_clean'),'w');
+for row=1:numel(C{1,1}) 
+    if any(row == delete)
+        %skip
+    else
+        fprintf(fid,'%s\r\n',C{1,1}{row,1}); 
+    end
+end
+fclose(fid);
+cd(out_dir)
+save('Rejected','Rejected')
+
+fclose all
+close all
+clear
+
+
+
